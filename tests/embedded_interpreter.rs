5c175bb861a483c76599d7b5512fbbb4c8af53c6
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-11-18 10:48:42 -0800

    Ran cargo fmt

--- tests/embedded_interpreter.rs
+++ tests/embedded_interpreter.rs
@@ -4,13 +4,13 @@ use tcltk_sys::*;
 struct Wrapper(*mut Tcl_Interp);
 
 impl Wrapper {
-	pub fn new() -> Wrapper {
-	unsafe {
-        let tcl_interp = Tcl_CreateInterp();
-        assert!(!tcl_interp.is_null());
-        Wrapper(tcl_interp)
+    pub fn new() -> Wrapper {
+        unsafe {
+            let tcl_interp = Tcl_CreateInterp();
+            assert!(!tcl_interp.is_null());
+            Wrapper(tcl_interp)
+        }
     }
-	}
 }
 
 impl Drop for Wrapper {
