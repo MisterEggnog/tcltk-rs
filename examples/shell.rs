2b5475229fbe00ccc8d2b3e4c0632181ddda7a31
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-11-18 11:03:16 -0800

    Shell echos now

--- examples/shell.rs
+++ examples/shell.rs
@@ -4,6 +4,13 @@ include!("../tests/embedded_interpreter.rs");
 
 fn main() -> io::Result<()> {
     let interpreter = Wrapper::new();
+    let ifs = io::stdin();
+    let handle = ifs.lock();
+
+    for line in handle.lines() {
+        let command_str = line?;
+        println!("{}", command_str);
+    }
 
     Ok(())
 }
