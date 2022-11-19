bbaa3630ebbe6b500a4a758389d02e9d0124a2a2
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-11-19 13:30:52 -0800

    Generates bindings file

--- tk-sys/build.rs
+++ tk-sys/build.rs
@@ -1,6 +1,23 @@
 use pkg_config::Config;
+use std::env;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=build.rs");
     Config::new().atleast_version("8.6").probe("tcl")?;
+
+    let bindings = bindgen::Builder::default()
+        .header("src/tk.h")
+        .clang_args(["-I", "/usr/include/tcl"])
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    println!("cargo:rerun-if-changed=src/tcl.h");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Unable to write bindings");
+
     Ok(())
 }
