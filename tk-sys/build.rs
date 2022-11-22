330f480a1cb89cde7dd70cfc7ef223ad70347161
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-11-22 15:46:26 -0800

    Partially invoke tcl shell in build script

--- tk-sys/build.rs
+++ tk-sys/build.rs
@@ -1,6 +1,8 @@
 use pkg_config::Config;
 use std::env;
+use std::io::Write;
 use std::path::PathBuf;
+use std::process::{Command, Stdio};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=build.rs");
@@ -18,5 +20,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Unable to write bindings");
 
+    set_library_path();
+
     Ok(())
+}
+
+fn set_library_path() {
+    // The only way I know how to find this is to invoke tcl & check it's
+    // output
+    let mut child_tclsh = Command::new("/usr/bin/env")
+        .arg("tclsh")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to find tclsh");
+
+    child_tclsh
+        .stdin
+        .take()
+        .expect("Failed to open stdin")
+        .write_all("puts $tcl_library".as_bytes())
+        .expect("Failed to write to stdin");
 }
