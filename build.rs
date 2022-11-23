0f019e3660f21644b51a730fc88ee0aa68060067
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-11-22 16:30:21 -0800

    Moved tcl locate function to correct build script

--- build.rs
+++ build.rs
@@ -4,7 +4,8 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Stdio, Command};
+use std::io::Write;
 
 fn token_name() -> PathBuf {
     [
@@ -36,6 +37,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     bindgen();
+
+    set_library_path();
 
     Ok(())
 }
@@ -117,4 +120,29 @@ fn bindgen() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Unable to write bindings");
+}
+
+fn set_library_path() {
+    // The only way I know how to find this is to invoke tcl & check it's
+    // output
+    let mut child_tclsh = Command::new("/usr/bin/env")
+        .arg("tclsh")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to find tclsh");
+
+    child_tclsh
+        .stdin
+        .take()
+        .expect("Failed to open stdin")
+        .write_all("puts $tcl_library".as_bytes())
+        .expect("Failed to write to stdin");
+
+    let output = child_tclsh
+        .wait_with_output()
+        .expect("Failed to read stdout");
+    let output = String::from_utf8(output.stdout).unwrap();
+
+    println!("cargo:rustc-env=TCL_LIBRARY={}", output);
 }
--- tk-sys/build.rs
+++ tk-sys/build.rs
@@ -20,32 +20,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Unable to write bindings");
 
-    set_library_path();
-
     Ok(())
-}
-
-fn set_library_path() {
-    // The only way I know how to find this is to invoke tcl & check it's
-    // output
-    let mut child_tclsh = Command::new("/usr/bin/env")
-        .arg("tclsh")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to find tclsh");
-
-    child_tclsh
-        .stdin
-        .take()
-        .expect("Failed to open stdin")
-        .write_all("puts $tcl_library".as_bytes())
-        .expect("Failed to write to stdin");
-
-    let output = child_tclsh
-        .wait_with_output()
-        .expect("Failed to read stdout");
-    let output = String::from_utf8(output.stdout).unwrap();
-
-    println!("cargo:rustc-env=tcl_library={}", output);
 }
