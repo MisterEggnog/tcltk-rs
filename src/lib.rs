10a43a23d48db36a8ed56e663ffb28ee04f1f558
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-11-22 16:39:18 -0800

    Load tcl library?

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub const TCL_LIBRARY: &str = env!("TCL_LIBRARY");
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,9 +1,11 @@
+use std::env::set_var;
 use tcl_sys::*;
 pub struct Wrapper(pub *mut Tcl_Interp);
 
 impl Wrapper {
     pub fn new() -> Wrapper {
         unsafe {
+            set_var("TCL_LIBRARY", TCL_LIBRARY);
             let tcl_interp = Tcl_CreateInterp();
             assert!(!tcl_interp.is_null());
             Wrapper(tcl_interp)
