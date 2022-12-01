4eb381145d36393ba1c2f0230da321c69d6861ae
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-12-01 15:30:25 -0800

    Specify where you can find Tcl documentation

--- tcl-sys/src/lib.rs
+++ tcl-sys/src/lib.rs
@@ -1,3 +1,8 @@
+//! Rust bindings for the Tcl interpreter
+//!
+//! Function specific documentation can be found at the [Tcl website]
+//!
+//! [Tcl website]: https://www.tcl-lang.org/man/tcl8.6/TclLib/contents.htm
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
