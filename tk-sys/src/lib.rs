a59b6a54686c2ae46b46031067b5117bda524a17
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-12-01 15:35:26 -0800

    Wrote documention for Tk library

--- tk-sys/src/lib.rs
+++ tk-sys/src/lib.rs
@@ -1,3 +1,12 @@
+//! Rust bindings for the Tk Graphics library
+//!
+//! Function specific documentation can be found at the [Tcl website]
+//!
+//! [Tcl website]: https://www.tcl-lang.org/man/tcl8.6/TkLib/contents.htm
+//!
+//! ## Note
+//! This library only seems to work if it's ran in an enviroment that has a
+//! graphics shell.
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
