efb1c995dd093b2ec6e31c536b4a51aaf3df9b05
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-12-01 16:40:46 -0800

    Removed code for local building
    Only for 0.1 release, will revert afterwards

--- tcl-sys/build.rs
+++ tcl-sys/build.rs
@@ -1,106 +1,16 @@
-use jobserver::Client;
 use pkg_config::Config;
 use std::env;
-use std::fs;
-use std::fs::File;
-use std::path::{Path, PathBuf};
-use std::process::Command;
-
-fn token_name() -> PathBuf {
-    [
-        env::var("OUT_DIR").unwrap(),
-        "TCL_SYS_CONFIGURED".to_string(),
-    ]
-    .iter()
-    .collect()
-}
-
-fn token_exists() -> bool {
-    Path::new(&token_name()).exists()
-}
-
-fn token_touch() -> anyhow::Result<()> {
-    File::create(token_name())?;
-    Ok(())
-}
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=build.rs");
 
     let include_paths = match Config::new().atleast_version("8.6").probe("tcl") {
         Ok(lib) => lib.include_paths,
-        Err(_) => {
-            copy_tcl_dir()?;
-            configure_unix()?;
-            token_touch()?;
-
-            build_unix()?;
-            vec![]
-        }
+        Err(_) => panic!("Currently does not support static building"),
     };
 
     bindgen(&include_paths);
-
-    Ok(())
-}
-
-fn copy_tcl_dir() -> anyhow::Result<()> {
-    if token_exists() {
-        return Ok(());
-    }
-
-    let out_dir = env::var("OUT_DIR")?;
-    Command::new("cp")
-        .args(["-Rf", "tcl8.6.12/", &out_dir])
-        .status()
-        .expect("Failed to execute process");
-
-    Ok(())
-}
-
-fn build_unix() -> anyhow::Result<()> {
-    let out_dir = env::var("OUT_DIR")?;
-    let work_dir = "tcl8.6.12/unix";
-    eprintln!("{}", out_dir);
-    let jobserver = unsafe { Client::from_env().unwrap() };
-
-    let mut cmd = Command::new("make");
-    jobserver.configure(&mut cmd);
-    let output = cmd
-        .current_dir(format!("{}/{}", out_dir, work_dir))
-        .output()
-        .expect("Failed to execute process");
-    println!("{}", String::from_utf8(output.stderr).unwrap());
-
-    let output = Command::new("make")
-        .arg("install")
-        .current_dir(format!("{}/{}", out_dir, work_dir))
-        .output()
-        .expect("Failed to execute process");
-    println!("{}", String::from_utf8(output.stderr).unwrap());
-
-    Ok(())
-}
-
-fn configure_unix() -> anyhow::Result<()> {
-    if token_exists() {
-        return Ok(());
-    }
-
-    let out_dir = env::var("OUT_DIR")?;
-    let work_dir = "tcl8.6.12/unix";
-    let command = fs::canonicalize(format!("{}/{}/configure", out_dir, work_dir)).expect(&format!(
-        "canonicalize failed for {out_dir}/{work_dir}/configure"
-    ));
-    eprintln!("canonicsed");
-    eprintln!("{:?}", command);
-    let output = Command::new(command)
-        .arg(format!("--prefix={}", out_dir))
-        .arg("--enable-share=no")
-        .current_dir(format!("{}/{}", out_dir, work_dir))
-        .output()
-        .expect("Failed to execute process");
-    println!("{}", String::from_utf8(output.stderr).unwrap());
 
     Ok(())
 }
