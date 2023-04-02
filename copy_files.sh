3516ee7636987b0f15e2b115e176e073d3effb56
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-04-02 15:02:56 -0700

    Copy tk headers for bundled

--- copy_files.sh
+++ copy_files.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-mkdir tcl-sys/tcl8.6.12
+rm -r tcl-sys/tcl8.6.12 tk-sys/tk8.6
 
+mkdir tcl-sys/tcl8.6.12
 cp -r tcl/generic tcl/compat tcl/libtommath tcl/macosx tcl/unix tcl/win tcl/library tcl/doc tcl-sys/tcl8.6.12
+
+mkdir tk-sys/tk8.6 tk-sys/tk8.6/generic
+cp -r tk/generic/*.h tk-sys/tk8.6/generic
