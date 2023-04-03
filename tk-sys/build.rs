45e8309281e3ba1f4f03e1bfd3946783448e558d
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-04-02 18:50:21 -0700

    Hopefully should be able to generate docs without tcl installed

--- tk-sys/build.rs
+++ tk-sys/build.rs
@@ -4,11 +4,14 @@ use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=build.rs");
-    println!(
-        "cargo:warning={}",
-        env::var("DEP_TCL_LOCAL_INCLUDE_DIR")
-            .unwrap_or("DEP_TCL_LOCAL_INCLUDE_DIR env var not found".to_string())
-    );
+    let tcl_local = env::var("DEP_TCL_LOCAL_INCLUDE_DIR")
+        .unwrap_or("DEP_TCL_LOCAL_INCLUDE_DIR env var not found".to_string());
+    println!("cargo:warning={}", &tcl_local);
+    if std::env::var("DOCS_RS").is_ok() {
+        do_bindgen(&[&tcl_local, "tk8.6/generic"].map(|f| PathBuf::from(f)));
+        return Ok(());
+    }
+
     let include_paths = Config::new()
         .atleast_version("8.6")
         .probe("tk")?
