e81aa0c128351aae2f045ecb89d1f0d1c8e121fe
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-04-02 20:40:19 -0700

    Copy only header files
    Part of removing local buildings for publishing

--- copy_files.sh
+++ copy_files.sh
@@ -3,7 +3,8 @@
 rm -r tcl-sys/tcl8.6 tk-sys/tk8.6
 
 mkdir tcl-sys/tcl8.6
-cp -r tcl/generic tcl/compat tcl/libtommath tcl/macosx tcl/unix tcl/win tcl/library tcl/doc tcl-sys/tcl8.6
+mkdir tcl-sys/tcl8.6/generic
+cp -r tcl/generic/*.h  tcl-sys/tcl8.6/generic
 
 mkdir tk-sys/tk8.6 tk-sys/tk8.6/generic
 cp -r tk/generic/*.h tk-sys/tk8.6/generic
